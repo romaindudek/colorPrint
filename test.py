ce7d17c57bdb810a42d9bdecbc6c9c74926ff8b9
romaindudek <pro@osezmedia.com>
2022-12-11 15:30:38 +0100

    Add test file

---
+++ test.py
@@ -0,0 +1,13 @@
+from colorPrint import cprint
+print()
+cprint("§_SUCCESS_SUCCESS")
+cprint("§_INFO_INFO")
+cprint("§_WARNING_WARNING")
+cprint("§_DANGER_DANGER")
+cprint("§_ERROR_ERROR")
+cprint("§-SUCCESS-SUCCESS")
+cprint("§-INFO-INFO")
+cprint("§-WARNING-WARNING")
+cprint("§-DANGER-DANGER")
+cprint("§-ERROR-ERROR")
+print()
