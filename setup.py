c4f1d8e47277fffefeb3bcd75281c76e5fa6d5bd
romaindudek <pro@osezmedia.com>
2023-03-22 17:56:58 +0100

    Librairie

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='color_print',
+    version='0.1',
+    description='Une librairie pour faire imprimer en couleur à l\'écran',
+    author='Romain Dudek',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
