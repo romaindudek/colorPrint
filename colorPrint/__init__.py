32df1038d5ad44911f8ef5a583b1b55607176063
romaindudek <pro@osezmedia.com>
2023-03-22 18:33:23 +0100

    Librairie init

---
+++ colorPrint/__init__.py
@@ -0,0 +1,1 @@
+from .colorPrint import cprint
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='color_print',
-    version='0.1',
-    description='Une librairie pour faire imprimer en couleur à l\'écran',
+    name='easy-colorprint',
+    version='0.1.3',
+    description='Une librairie pour imprimer en couleur à l\'écran',
     author='Romain Dudek',
     packages=find_packages(),
     install_requires=[
